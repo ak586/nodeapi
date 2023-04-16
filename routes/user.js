8fe5b2754b04794eee5e49b1bf54eb8d0e7be303
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2023-04-16 21:52:52 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,15 @@ const router = express.Router();
 router.get('/all', getAllUsers);
 router.post('/new', createNewUser);
 router.get('/userid/special', specialUser)
+
 //dynamic routing
 
-
+/*this is same as the next routing
 router.get("/userid/:id", getUserDetails);
 router.put("/userid/:id", updateUser);
 router.delete("/userid/:id", deleteUser);
-
-
+*/
+//same as the above
 router.route("/userid/:id")
     .get(getUserDetails).
     put(updateUser)
